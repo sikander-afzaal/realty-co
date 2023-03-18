a05d076286deeadecc80ed4e12e27eec452840b6
sikander-afzaal <sikander.aaa@gmail.com>
2023-03-18 13:44:48 +0500

    asdasd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./layout/Header";
 import Home from "./pages/home/index";
+import Blog from "./pages/blog/index";
 
 const App = () => {
   return (
     <>
       <Header />
-      <Home />
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<Blog />} path="/blog" />
+      </Routes>
     </>
   );
 };
